b86c2ff5ce882b09dca54109eb02e874a718bae4
Kyler Mintah <kylermintah@gmail.com>
2021-09-26 14:18:00 -0400

    Stripe Integration

--- app.js
+++ app.js
@@ -1,69 +1,103 @@
-const express = require('express');
-const path = require('path');
+const express = require('express')
+const path = require('path')
 const exphbs = require('express-handlebars')
-const stripe = require('stripe');
+require('dotenv').config();
+const Stripe = require('stripe')
+const stripe = Stripe(
+  process.env.STRIPE_SECRET_KEY,
+)
 
-var app = express();
+var app = express()
 
 // view engine setup (Handlebars)
-app.engine('hbs', exphbs({
-  defaultLayout: 'main',
-  extname: '.hbs'
-}));
-app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, 'public')));
+app.engine(
+  'hbs',
+  exphbs({
+    defaultLayout: 'main',
+    extname: '.hbs',
+  }),
+)
+app.set('view engine', 'hbs')
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 /**
  * Home route
  */
-app.get('/', function(req, res) {
-  res.render('index');
-});
+app.get('/', function (req, res) {
+  res.render('index')
+})
 
 /**
  * Checkout route
  */
-app.get('/checkout', function(req, res) {
+app.get('/checkout', async function (req, res) {
   // Just hardcoding amounts here to avoid using a database
-  const item = req.query.item;
-  let title, amount, error;
+  const item = req.query.item
+  var title, amount, error
 
   switch (item) {
     case '1':
-      title = "The Art of Doing Science and Engineering"
-      amount = 2300      
-      break;
+      title = 'The Art of Doing Science and Engineering'
+      amount = 2300
+      break
     case '2':
-      title = "The Making of Prince of Persia: Journals 1985-1993"
+      title = 'The Making of Prince of Persia: Journals 1985-1993'
       amount = 2500
-      break;     
+      break
     case '3':
-      title = "Working in Public: The Making and Maintenance of Open Source"
-      amount = 2800  
-      break;     
+      title = 'Working in Public: The Making and Maintenance of Open Source'
+      amount = 2800
+      break
     default:
       // Included in layout view, feel free to assign error
-      error = "No item selected"      
-      break;
+      error = 'No item selected'
+      break
   }
+
+  let params = {
+    amount: amount,
+    currency: 'usd',
+    metadata: { integration_check: 'accept_a_payment' },
+    payment_method_types: ['card'],
+  }
+
+  // Creating payment intent server-side
+  const paymentIntent = await stripe.paymentIntents.create(params)
 
   res.render('checkout', {
     title: title,
     amount: amount,
-    error: error
-  });
-});
+    error: error,
+    client_secret: paymentIntent.client_secret,
+  })
+})
 
 /**
- * Success route
+ * Success GET route
  */
-app.get('/success', function(req, res) {
-  res.render('success');
-});
+app.get('/success', function (req, res) {
+  res.status(404).send('<h1>404 - Page Not Found<h1>')
+})
+
+/**
+ * Success POST route
+ */
+app.post('/success', function (req, res) {
+  res.render('success', req.body) 
+})
+
+/**
+ * Config route
+ */
+ app.get('/config', function (req, res) {
+  res.send({publishiableKey: process.env.STRIPE_PUBLISHABLE_KEY})
+})
 
 /**
  * Start server
  */
 app.listen(3000, () => {
-  console.log('Getting served on port 3000');
-});
+  console.log('Getting served on port 3000')
+})
--- public/js/custom.js
+++ public/js/custom.js
@@ -2,13 +2,107 @@
  * Clientside helper functions
  */
 
-$(document).ready(function() {
-  var amounts = document.getElementsByClassName("amount");
+$(document).ready(function () {
+  var amounts = document.getElementsByClassName('amount')
 
   // iterate through all "amount" elements and convert from cents to dollars
   for (var i = 0; i < amounts.length; i++) {
-    amount = amounts[i].getAttribute('data-amount') / 100;  
-    amounts[i].innerHTML = amount.toFixed(2);
+    amount = amounts[i].getAttribute('data-amount') / 100
+    amounts[i].innerHTML = amount.toFixed(2)
   }
 })
 
+// Card error display helper function
+function displayErrorMessage(message) {
+  var displayError = document.getElementById('card-errors')
+  displayError.textContent = message
+}
+
+// Additional Stripe payment scripts
+$(document).ready(async function () {
+  const { publishiableKey } = await fetch('/config').then((r) => r.json())
+  if (!publishiableKey) {
+    console.log('Stripe API key not found')
+    alert(
+      'Stripe API key not found - Please set your Stripe publishiable key in the .env file',
+    )
+  }
+
+  const stripeApiKey = publishiableKey
+
+  // Checkout page script
+  if (top.location.pathname === '/checkout') {
+    var stripe = Stripe(stripeApiKey)
+    // Set up Stripe.js and Elements to use in checkout form
+    var elements = stripe.elements()
+    var style = {
+      base: {
+        color: '#32325d',
+      },
+    }
+
+    var card = elements.create('card', { style: style })
+
+    card.mount('#card-element')
+    card.on('change', function (event) {
+      if (event.error) {
+        displayErrorMessage(event.error.message)
+      } else {
+        displayErrorMessage('')
+      }
+    })
+
+    var form = document.getElementById('checkout-form')
+    form.addEventListener('submit', async function (ev) {
+      ev.preventDefault()
+      // If the client secret was rendered server-side as a data-secret attribute
+      // on the <form> element, you can retrieve it here by calling `form.dataset.secret`
+      // getting the value
+      var email = $('#email').val()
+      var client_secret = form.dataset.secret
+      console.log('confirming payment...')
+      await stripe
+        .confirmCardPayment(client_secret, {
+          payment_method: {
+            card: card,
+            billing_details: {
+              email: email,
+            },
+          },
+        })
+        .then(function (result) {
+          if (result.error) {
+            // Display payment error to user
+            displayErrorMessage(result.error.message)
+          } else {
+            // The payment has been processed!
+            if (result.paymentIntent.status === 'succeeded') {
+              // Show redirect to payment success page with form submission action
+              console.log('payment succeeded!')
+              $('<input />')
+                .attr('type', 'hidden')
+                .attr('name', 'client_secret')
+                .attr('value', client_secret)
+                .appendTo(form)
+              form.submit()
+            }
+          }
+        })
+    })
+  }
+
+  // Success page script
+  if (top.location.pathname === '/success') {
+    var paymentDetails = document.getElementById('payment-details')
+    var stripe = Stripe(stripeApiKey)
+    const paymentIntent = (
+      await stripe.retrievePaymentIntent(paymentDetails.dataset.secret)
+    ).paymentIntent
+    console.log(`The payment intent is ${JSON.stringify(paymentIntent)}`)
+    $('#payment-details').html(
+      `<div class='mt-20 text-info'>ID: ${paymentIntent.id} <br/> Total: $${(
+        paymentIntent.amount / 100
+      ).toFixed(2)}</span></div>`,
+    )
+  }
+})
